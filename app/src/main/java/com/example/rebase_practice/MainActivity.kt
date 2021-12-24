15896340c8a8324ac91373dceb2b8fe4348a5257
jjeewon <wldnjs594@gmail.com>
2021-12-24 10:52:27 +0900

    feat - update master

--- app/src/main/java/com/example/rebase_practice/MainActivity.kt
+++ app/src/main/java/com/example/rebase_practice/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.rebase_practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val a = 3
+        val b = 4
+        val c = a + b
     }
 }
